9a00cbbfd7c143a9bfcbb21cfbd0b489363cd754
Santiago Jaimes <sa.jaimesp@uniandes.edu.co>
2024-10-12 16:52:20 -0500

    cambios en Predecir

--- frontend/src/components/Predecir.js
+++ frontend/src/components/Predecir.js
@@ -10,17 +10,37 @@ import Card from 'react-bootstrap/Card';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip as ChartTooltip,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, ChartTooltip);
+
 const Predecir = () => {
-  const [texts, setTexts] = useState([]); // Textos ingresados manualmente
-  const [predictions, setPredictions] = useState([]); // Predicciones obtenidas del endpoint
-  const [probabilities, setProbabilities] = useState([]); // Probabilidades obtenidas del endpoint
+  const [texts, setTexts] = useState([]); 
+  const [predictions, setPredictions] = useState([]); 
+  const [probabilities, setProbabilities] = useState([]); 
   const [loading, setLoading] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0); // Índice para resultados
-  const [showTextInputs, setShowTextInputs] = useState(false); // Controla la visualización de cuadros de texto
-  const [showCSVInput, setShowCSVInput] = useState(false); // Controla la visualización del campo de CSV
-  const [errorMessage, setErrorMessage] = useState(''); // Mensaje de error para campos vacíos
-  const [emptyFields, setEmptyFields] = useState([]); // Estado para controlar qué campos están vacíos
-  const [displayTexts, setDisplayTexts] = useState([]); // Textos mostrados en la Card después de predecir
+  const [currentIndex, setCurrentIndex] = useState(0); 
+  const [showTextInputs, setShowTextInputs] = useState(false); 
+  const [showCSVInput, setShowCSVInput] = useState(false); 
+  const [errorMessage, setErrorMessage] = useState(''); 
+  const [emptyFields, setEmptyFields] = useState([]); 
+  const [displayTexts, setDisplayTexts] = useState([]); 
+
+  const genericReasons = {
+    3: 'Tu texto ha sido clasificado en el ODS 3 (Salud y Bienestar). Esto significa que el contenido del texto está relacionado con aspectos que afectan la salud física o mental de las personas, y cómo mejorar su bienestar.',
+    4: 'Tu texto ha sido clasificado en el ODS 4 (Educación de Calidad). Esto sugiere que el contenido está vinculado con la mejora de la educación, el acceso a oportunidades educativas o la igualdad en la educación.',
+    5: 'Tu texto ha sido clasificado en el ODS 5 (Igualdad de Género). Esto implica que el contenido trata sobre temas relacionados con la igualdad entre géneros, el empoderamiento de las mujeres o la eliminación de la discriminación de género.'
+  };
+
+  // Nuevo estado para el índice de búsqueda
+  const [searchIndex, setSearchIndex] = useState('');
 
   // Agregar una nueva caja de texto (máximo 5)
   const addTextInput = () => {
@@ -51,15 +71,15 @@ const Predecir = () => {
 
   // Manejar carga de archivo CSV
   const handleCSVUpload = (data) => {
-    const csvTexts = data.map(row => row[0]).filter(text => text.trim() !== ''); // Omite textos vacíos
+    const csvTexts = data.map(row => row[0]).filter(text => text.trim() !== ''); 
     setTexts(csvTexts);
   };
 
   // Enviar los textos al backend para la predicción
   const handlePredict = async () => {
-    const savedIndex = currentIndex; // Guardar el índice actual
+    const savedIndex = currentIndex; 
   
-    setErrorMessage(''); // Limpiar mensaje de error
+    setErrorMessage(''); 
     let hasError = false;
     const newEmptyFields = [];
 
@@ -98,7 +118,6 @@ const Predecir = () => {
       setPredictions(data.predictions);
       setProbabilities(data.probabilities);
       setDisplayTexts([...texts]);
-      // Mantener el currentIndex en el valor guardado previamente
       setCurrentIndex(savedIndex); 
     } catch (error) {
       console.error('Error al realizar la predicción:', error);
@@ -113,6 +132,27 @@ const Predecir = () => {
     } else if (direction === 'prev') {
       setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
     }
+  };
+
+  // Manejar cambio en el campo de búsqueda por índice
+  const handleSearchIndexChange = (e) => {
+    setSearchIndex(e.target.value);
+  };
+
+  // Manejar ir al índice especificado
+  const handleGoToIndex = () => {
+    const index = parseInt(searchIndex, 10);
+    if (!isNaN(index) && index >= 1 && index <= predictions.length) {
+      setCurrentIndex(index - 1);
+    } else {
+      alert(`Por favor, ingresa un número entre 1 y ${predictions.length}`);
+    }
+  };
+
+  // Manejador para prevenir el comportamiento predeterminado del formulario
+  const handleSearchSubmit = (e) => {
+    e.preventDefault(); // Prevenir recarga de la página
+    handleGoToIndex();  // Ejecutar la función para ir al índice especificado
   };
 
   // Imágenes de los ODS
@@ -145,27 +185,27 @@ const Predecir = () => {
       {/* Botones para mostrar inputs */}
       <Row className="justify-content-center mt-4">
         <Col sm="8" className="text-center">
-        <OverlayTrigger
-          placement="top"
-          overlay={
-            <Tooltip id="tooltip-add-text">
-              Máximo se pueden agregar 5 textos por este método
-            </Tooltip>
-          }
-        >
-          <Button
-            variant="primary"
-            onClick={() => {
-              setShowTextInputs(true);
-              setShowCSVInput(false);
-              addTextInput();
-            }}
-            className="action-btn"
-            disabled={showCSVInput || texts.length >= 5}
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip id="tooltip-add-text">
+                Máximo se pueden agregar 5 textos por este método
+              </Tooltip>
+            }
           >
-            Agregar texto
-          </Button>
-        </OverlayTrigger>
+            <Button
+              variant="primary"
+              onClick={() => {
+                setShowTextInputs(true);
+                setShowCSVInput(false);
+                addTextInput();
+              }}
+              className="action-btn"
+              disabled={showCSVInput || texts.length >= 5}
+            >
+              Agregar texto
+            </Button>
+          </OverlayTrigger>
           <Button
             variant="secondary"
             onClick={() => {
@@ -190,7 +230,7 @@ const Predecir = () => {
                 placeholder={`Texto ${index + 1}`}
                 value={text}
                 onChange={(e) => handleTextChange(index, e)}
-                className={`text-input ${emptyFields.includes(index) ? 'is-invalid' : ''}`} // Muestra en rojo solo los campos vacíos
+                className={`text-input ${emptyFields.includes(index) ? 'is-invalid' : ''}`} 
               />
               <Button
                 variant="danger"
@@ -236,80 +276,120 @@ const Predecir = () => {
       {predictions.length > 0 && (
         <Row className="justify-content-center mt-4">
           <hr className="custom-hr"/>
-          <Col sm="12" className="text-center">
-            <h3 className="resultados">
-              Resultados
-            </h3>
-            <div className="d-flex justify-content-between align-items-center" style={{ width: '100%', position: 'relative' }}>
-              <Button
-                className="nav-button"
-                onClick={() => handleNavigation('prev')}
-                disabled={currentIndex === 0} // Deshabilita si está en el primer resultado
-                style={{ borderRadius: '50%', backgroundColor: 'black', position: 'absolute', left: '100px' }} // Flecha izquierda fuera del área
-              >
-                <span style={{ color: 'white' }}>&lt;</span>
-              </Button>
-
-              <div className="result-container d-flex" style={{ width: '90%', maxWidth: '1200px', marginLeft: '170px' }}>
-                <div style={{ flex: '1', maxWidth: '50%' }}> {/* Ocupa el 60% para la gráfica */}
-                  <Bar
-                    data={{
-                      labels: ['ODS 3', 'ODS 4', 'ODS 5'], // Etiquetas según los ODS predichos
-                      datasets: [
-                        {
-                          label: 'Probabilidad',
-                          data: probabilities[currentIndex], // Probabilidades del texto actual
-                          backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 206, 86, 0.2)'],
-                          borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 206, 86, 1)'],
-                          borderWidth: 1,
-                        },
-                      ],
-                    }}
-                    options={{
-                      scales: {
-                        y: {
-                          beginAtZero: true,
-                          max: 1,
-                        },
-                      },
-                    }}
-                    className="chart-bar"
+          <Col sm="12">
+            {/* Título y campo de búsqueda centrados */}
+            <Row className="align-items-center justify-content-center text-center">
+              <Col xs={12} md="auto">
+                <h3 className="resultados">
+                  Resultados
+                </h3>
+              </Col>
+              {/* Campo para buscar por índice */}
+              <Col xs={12} md="auto">
+                <Form onSubmit={handleSearchSubmit} className="d-flex align-items-center justify-content-center">
+                  <Form.Label className="mr-2">Ir al resultado número:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    min="1"
+                    max={predictions.length}
+                    value={searchIndex}
+                    onChange={handleSearchIndexChange}
+                    style={{ width: '100px' }}
                   />
-                </div>
+                  <Button variant="primary" onClick={handleGoToIndex} className="ml-2">Ir</Button>
+                </Form>
+              </Col>
+            </Row>
 
-                <Card className="result-card" style={{ flex: '1', maxWidth: '40%', marginLeft: '20px' }}>
-                  <Card.Body>
-                    <Row>
-                      {/* Primera columna: Texto con scroll */}
-                      <Col xs={6} style={{ maxHeight: '250px', overflowY: 'auto' }}>
-                        <h5>Texto {currentIndex + 1}:</h5>
-                        <p>{displayTexts[currentIndex]}</p>
-                      </Col>
+            <Row className="justify-content-center mt-4">
+              <Col xs="auto">
+                <Button
+                  className="nav-button"
+                  onClick={() => handleNavigation('prev')}
+                  disabled={currentIndex === 0} 
+                  style={{ borderRadius: '50%', backgroundColor: 'black' }} 
+                >
+                  <span style={{ color: 'white' }}>&lt;</span>
+                </Button>
+              </Col>
 
-                      {/* Segunda columna: Predicción e imagen del ODS */}
-                      <Col xs={6} className="text-center">
-                        <h5>Predicción:</h5>
-                        <img 
-                          src={imageURL} 
-                          alt={`ODS ${predictions[currentIndex]}`} 
-                          style={{ width: '200px', height: '200px' }}
-                        />
-                      </Col>
-                    </Row>
-                  </Card.Body>
-                </Card>
+              <Col xs={12} md={8}>
+                <Row className="result-container">
+                  <Col xs={12} md={6} className="mb-3">
+                    <Bar
+                      data={{
+                        labels: ['ODS 3', 'ODS 4', 'ODS 5'], 
+                        datasets: [
+                          {
+                            label: 'Probabilidad',
+                            data: probabilities[currentIndex], 
+                            backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+                            borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 206, 86, 1)'],
+                            borderWidth: 1,
+                          },
+                        ],
+                      }}
+                      options={{
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                          y: {
+                            beginAtZero: true,
+                            max: 1,
+                          },
+                        },
+                      }}
+                      className="chart-bar"
+                    />
+                  </Col>
 
-              </div>
+                  <Col xs={12} md={6}>
+                    <Card className="result-card">
+                      <Card.Body>
+                        <Row>
+                          {/* Primera columna: Texto con scroll */}
+                          <Col xs={6} style={{ maxHeight: '250px', overflowY: 'auto' }}>
+                            <h5>Texto {currentIndex + 1}:</h5>
+                            <p>{displayTexts[currentIndex]}</p>
+                          </Col>
 
-              <Button
-                className="nav-button"
-                onClick={() => handleNavigation('next')}
-                disabled={currentIndex === predictions.length - 1} // Deshabilita si está en el último resultado
-                style={{ borderRadius: '50%', backgroundColor: 'black', position: 'absolute', right: '100px' }} // Flecha derecha fuera del área
-              >
-                <span style={{ color: 'white' }}>&gt;</span>
-              </Button>
-            </div>
+                          {/* Segunda columna: Predicción e imagen del ODS */}
+                          <Col xs={6} className="text-center">
+                            <h5>Predicción:</h5>
+                            <img 
+                              src={imageURL} 
+                              alt={`ODS ${predictions[currentIndex]}`} 
+                              style={{ width: '200px', height: '200px' }}
+                            />
+                          </Col>
+                        </Row>
+
+                        {/* Texto genérico basado en la predicción */}
+                        <Row className="mt-3">
+                          <Col>
+                            <p style={{ textAlign: 'justify' }}>
+                              {genericReasons[predictions[currentIndex]]}
+                            </p>
+                          </Col>
+                        </Row>
+
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              </Col>
+
+              <Col xs="auto">
+                <Button
+                  className="nav-button"
+                  onClick={() => handleNavigation('next')}
+                  disabled={currentIndex === predictions.length - 1} 
+                  style={{ borderRadius: '50%', backgroundColor: 'black' }} 
+                >
+                  <span style={{ color: 'white' }}>&gt;</span>
+                </Button>
+              </Col>
+            </Row>
           </Col>
         </Row>
       )}
